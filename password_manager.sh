6b35e4cbd4958ebb16bcb434f26708e93a0404c9
hellonagi <38994662+hellonagi@users.noreply.github.com>
2023-12-06 10:25:22 +0900

    Add main manu for password manager

--- password_manager.sh
+++ password_manager.sh
@@ -1,16 +1,40 @@
 #!/usr/bin/bash
 
+function add_password (){
+    echo "サービス名を入力してください："
+    read service
+
+    echo "ユーザー名を入力してください："
+    read username
+
+    echo "パスワードを入力してください："
+    read -s password
+
+    echo "${service}:${username}:${password}" >> accounts.txt
+}
+
 echo "パスワードマネージャーへようこそ！"
 
-echo "サービス名を入力してください："
-read service
+while true
+do
+    echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
+    read choice
 
-echo "ユーザー名を入力してください："
-read username
-
-echo "パスワードを入力してください："
-read -s password
-
-echo "${service}:${username}:${password}" >> accounts.txt
-
-echo "Thank you!"
+    case $choice in
+        "Add Password" )
+            add_password
+            echo "パスワードの追加は成功しました。"
+            ;;
+        "Get Password" )
+            # TODO: get_passwordの実装
+            echo "Get Password"
+            ;;
+        "Exit" )
+            echo "Thank you!"
+            break
+            ;;
+        * )
+            echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+            ;;
+    esac
+done
